d29ec9587747462fc597f36fa2284c1b4231d4ec
Leony Brok <leonybrok@gmail.com>
2018-07-28 02:12:56 +0000

    Style improvements

---
+++ static/main.js
@@ -0,0 +1,11 @@
+/* * * * * * *
+* Assign "active" class to navbar item based on current page.
+*
+* By Dave Rogers.
+* https://gist.github.com/daverogers/5375778
+* * * * * * */
+$(document).ready(function() {
+	// get current URL path and assign 'active' class
+	var pathname = window.location.pathname;
+	$('.nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+})
